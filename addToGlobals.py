7615b06c32a7549f3def5cf73be401442a91b947
Matthew <smith_matthew@live.com>
2015-03-07 16:43:43 -0500

    Got the text region around the user's cursor to be added to the global comment

--- addToGlobals.py
+++ addToGlobals.py
@@ -7,4 +7,7 @@ class add_to_globalCommand(sublime_plugin.TextCommand):
         return '/*global */'
 
     def run(self, edit):
-        self.view.insert(edit, 0, "/*global */")
+        r = self.view.sel()[0]
+        word = self.view.word(r)
+        actual = self.view.substr(word)
+        self.view.insert(edit, 0, "/*global " + actual + "*/\n")
