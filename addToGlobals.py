2897bb54a3423e95a03fad81d3cdafa076fd9e0f
Matthew <smith_matthew@live.com>
2015-03-11 16:25:36 -0400

    the single line commits didn't work

--- addToGlobals.py
+++ addToGlobals.py
@@ -10,4 +10,4 @@ class add_to_globalCommand(sublime_plugin.TextCommand):
         r = self.view.sel()[0]
         word = self.view.word(r)
         actual = self.view.substr(word)
-        self.view.insert(edit, 0, "//global " + actual + "\n")
+        self.view.insert(edit, 0, "/*global " + actual + "*/\n")
