7e602190b55487a964dab4987ed35ae175931cb0
Michael Hall <github@michaelhall.ca>
2015-04-22 21:59:21 -0400

    Update the add globals command.
    - Add all selected words.
    - Don’t add duplicates.

--- addToGlobals.py
+++ addToGlobals.py
@@ -1,13 +1,35 @@
 import sublime
 import sublime_plugin
+import re
+
+reNewline = r'\r|\n'
+reGlobals = r'(?:\/*global )([^*]*)(?:\*\/)'
+reGlobalSplit = r',\s*'
 
 class add_to_globalCommand(sublime_plugin.TextCommand):
 
-    def getComment():
-        return '/*global */'
+    def getContents(self):
+        region = sublime.Region(0, self.view.size())
+        return self.view.substr(region)
+
+    def getImports(self):
+        imports = []
+        contents = re.sub(reNewline, '', self.getContents())
+        matches = re.findall(reGlobals, contents)
+        for m in matches:
+            print(m)
+            names = re.split(reGlobalSplit, m)
+            for n in names:
+                if n not in imports:
+                    imports.append(n)
+        return imports
 
     def run(self, edit):
-        r = self.view.sel()[0]
-        word = self.view.word(r)
-        actual = self.view.substr(word)
-        self.view.insert(edit, 0, "/*global " + actual + "*/\n")
+        imports = self.getImports()
+        sel = self.view.sel()
+        for s in sel:
+            word = self.view.word(s)
+            actual = self.view.substr(word)
+            if actual not in imports:
+                self.view.insert(edit, 0, "/*global " + actual + "*/\n")
+                imports.append(actual)
